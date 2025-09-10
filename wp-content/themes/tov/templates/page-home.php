7269543c6c47d0b7dad1dafb54b5492ede096f4b
AlishaDarly-mountbox <alisha.d@mountbox.in>
2025-09-10 13:08:41 +0530

    check

--- wp-content/themes/tov/templates/page-home.php
+++ wp-content/themes/tov/templates/page-home.php
@@ -12,15 +12,7 @@ get_header(); ?>
     <?php while (have_posts()) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class('homepage-article'); ?>>
             <div class="homepage-content-wrapper">
-                <h1>Welcome to our website</h1>
-                <h2>Welcome to our website</h2>
-                <h3>Welcome to our website</h3>
-                <h4>Welcome to our website</h4>
-                <h5>Welcome to our website</h5>
-                <h6>Welcome to our website</h6>
-                <p>Welcome to our website</p>
-                <a href="#">Welcome to our website</a>
-                <button class="btn btn-primary">know more</button>
+                
                 <?php
                 
                 // Display the page content which will contain shortcodes
